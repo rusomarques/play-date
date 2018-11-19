af8d822e212d8ed829f0a80bddf0988f37a2e3d1
1334 <1334@shadowproject.net>
2018-11-19 09:19:57 +0100

    style: remove commented code and extract state to separate file

--- client/src/redux/rest.js
+++ client/src/redux/rest.js
@@ -22,33 +22,11 @@ export const rest = reduxApi({
       method: 'POST'
     }
   }
-
-  // updateEvent: {
-  //   url: '/events/:id',
-  //   options: {
-  //     method: 'PATCH'
-  //   }
-  // },
-  // deleteEvent: {
-  //   url: '/events/:id',
-  //   options: {
-  //     method: 'DELETE'
-  //   }
-  // },
-  // login: {
-  //   url: '/login',
-  //   options: {
-  //     method: 'POST'
-  //   }
-  // }
 });
 
 rest.use('fetch', adapterFetch(fetch));
 rest.use('rootUrl', 'http://localhost:3002');
-// rest.use('rootUrl', 'http://localhost:3000');
 rest.use('options', {
-  // body: JSON.stringify(event),
-
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json'
